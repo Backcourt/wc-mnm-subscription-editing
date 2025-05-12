b7e182e644a053117607e8a7dca302dff95bae8a
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2025-05-12 19:42:25 -0400

    remove wc_mnm_edit_container_order_item_in_shop_subscription template callback. Handled by variable MNM plugin now and causes fatal error if that is not active.

--- includes/wc-mnm-subscription-editing-template-hooks.php
+++ includes/wc-mnm-subscription-editing-template-hooks.php
@@ -7,9 +7,6 @@
 
 defined( 'ABSPATH' ) || exit;
 
-// Edit container form - stripped down add to cart form.
-add_action( 'wc_mnm_edit_container_order_item_in_shop_subscription', 'wc_mnm_template_edit_variable_container_order_item', 10, 4 );
-
 // Port add to cart elements.wc_mnm_template_edit_subscription_headling
 add_action( 'wc_mnm_before_edit_container_order_item_form', 'wc_mnm_template_edit_subscription_heading', 10, 4 );
 add_action( 'wc_mnm_edit_container_order_item_content', 'wc_mnm_template_edit_container_button', 40, 4 );
